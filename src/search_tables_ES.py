0dca0ead8a3b8ab885c98c6674ac3f4b5be75619
vendi <svitlana.vakulenko@gmail.com>
2017-08-03 00:07:17 +0200

    new: single question test for cell search with reindexing option

--- src/search_tables_ES.py
+++ src/search_tables_ES.py
@@ -7,10 +7,15 @@ author: svakulenko
 from elasticsearch import Elasticsearch
 
 from settings import *
+from mappings import ngram_tokenizer
+from index_tables_ES import cells2ES
 
 QUESTIONS = ['what is the population of linz?',
              'what is the immigration in linz?',
              'what is the internal_mig_immigration in linz?']  # correct answer: 9693
+
+THRESHOLD = 2
+
 
 class ESConnection():
     def __init__(self, index_name, doc_type):
@@ -21,7 +26,7 @@ class ESConnection():
         self.index_name = index_name
         self.doc_type = doc_type
 
-    def search_tables(self, question, explain=False):
+    def search_tables(self, question, explain=True):
         results = self.es.search(index=self.index_name, body={"query": {"match": {"content": question}}}, doc_type=self.doc_type, explain=explain)['hits']['hits']
         return results
 
@@ -52,8 +57,23 @@ def test_search_rows_qs(index_name=INDEX_NAME_ROWS, doc_type=DOC_TYPE_ROWS):
         print results[0]['_score']
 
 
-def test_search_cells_qs(questions=QUESTIONS, index_name=INDEX_NAME_CELLS, doc_type=DOC_TYPE_CELLS):
+def test_search_cells_q(index_name=INDEX_NAME_CELLS, doc_type=DOC_TYPE_CELLS, reindex=False):
     es = ESConnection(index_name, doc_type)
+
+    question = 'lin'
+
+    if reindex:
+        cells2ES(SAMPLE_CSV_FILE, index_name, mapping=ngram_tokenizer)
+
+    results = es.search_tables(question)
+    print results
+
+
+def test_search_cells_qs(questions=QUESTIONS, index_name=INDEX_NAME_CELLS, doc_type=DOC_TYPE_CELLS, reindex=False):
+    es = ESConnection(index_name, doc_type)
+
+    if reindex:
+        cells2ES(SAMPLE_CSV_FILE, index_name, mapping={})
 
     for question in questions:
         print question
@@ -63,19 +83,30 @@ def test_search_cells_qs(questions=QUESTIONS, index_name=INDEX_NAME_CELLS, doc_t
         # print results
         for result in results:
             # print result
-            print result['_score']
+            score = result['_score']
+            if score < THRESHOLD:
+                continue
 
             value = result['_source']['content']
             row = result['_source']['row']
             column = result['_source']['column']
-            print "row %s column %s value %s" % (row, column, value)
 
-            # hit the header
+            # hit the header - report the column with the top score
             if row == 0:
-                j = column
-            # hit the row
+                if j == None:
+                    j = column
+                    # report column cell found
+                    print score
+                    print "row %s column %s value %s" % (row, column, value)
+
+            # hit the top scored row
             else:
-                i = row
+                if i == None:
+                    i = row
+                    # report row cell found
+                    print score
+                    print "row %s column %s value %s" % (row, column, value)
+
             # look up the cell value with the answer
 
         # found row and column pointers
@@ -84,6 +115,8 @@ def test_search_cells_qs(questions=QUESTIONS, index_name=INDEX_NAME_CELLS, doc_t
             # show top result cell content as the final answer
             value = results[0]['_source']['content']
             print "Answer: row %s column %s value %s" % (i, j, value)
+        else:
+            print "Answer: Not found"
 
 
         print '\n'
