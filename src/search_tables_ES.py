ae76c3706cdbb8f2e95cf3b8c2a7c0881ea4155e
vendi <svitlana.vakulenko@gmail.com>
2017-08-02 15:00:29 +0200

    new: row search test with multiple questions

--- src/search_tables_ES.py
+++ src/search_tables_ES.py
@@ -8,6 +8,10 @@ from elasticsearch import Elasticsearch
 
 from settings import *
 
+QUESTIONS = ['what is the population of linz?',
+             'what is the immigration in linz?',
+             'what is the internal_mig_immigration of linz?']
+
 
 def search_rows(index_name, question, explain=True):
     es = Elasticsearch()
@@ -24,5 +28,15 @@ def test_search_rows():
     print results['hits']['hits'][0]['_score']
 
 
+def test_search_rows_w_qs(questions=QUESTIONS):
+    for question in questions:
+        print question
+        results = search_rows(INDEX_NAME, question)
+        # print results
+        print results['hits']['hits'][0]['_score']
+        print results['hits']['hits'][0]['_source']['row']
+        print '\n'
+
+
 if __name__ == '__main__':
-    test_search_rows()
+    test_search_rows_w_qs()
